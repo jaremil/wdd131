0cb112e1b0da4a149ba64ccb1c5af46061f6537b
Jade Miller <mil19017@byui.edu>
2024-09-25 12:23:24 -0600

    actually finished

--- mission/mission.js
+++ mission/mission.js
@@ -9,11 +9,14 @@ if (themeValue == "dark") {
   document.body.setAttribute('data-theme', 'dark'); 
 
   document.querySelector('#whiteLogo').setAttribute('src', 'byui-logo_white.png');
-  document.querySelector('#blueLogo').setAttribute('src', ''); 
+  document.querySelector('#blueLogo').removeAttribute('src');
+  document.querySelector('#blueLogo').removeAttribute('alt');
+  
   } else {
     document.body.setAttribute('data-theme', 'light'); 
     document.querySelector('#blueLogo').setAttribute('src', 'byui-logo_blue.jpg');
-    document.querySelector('#whiteLogo').setAttribute('src', ''); 
+    document.querySelector('#whiteLogo').removeAttribute('src');
+    document.querySelector('#whiteLogo').removeAttribute('alt');
   }
 }
 
