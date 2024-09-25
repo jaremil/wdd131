634d003876e6174b41da4e8bec6c51a0b88f7f9d
Jade Miller <mil19017@byui.edu>
2024-09-25 09:49:08 -0600

    class work

---
+++ mission/mission.js
@@ -0,0 +1,21 @@
+var changeToggle = document.getElementById("changeTheme");
+
+const changeToggle = document.querySelector(".dark");
+
+function changeTheme(){
+
+    var select = document.querySelector("select");
+    select.value; 
+
+if (arguments.value == "dark") {
+
+    // add the dark class to the body
+   // change the source of the logo img to point to the white logo.
+
+  } else {
+   // remove the dark class
+   // make sure the logo src is the blue logo.
+  }
+}
+
+changeToggle.addEventListener("change", changeTheme);
