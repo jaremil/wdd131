b4855c2884c1186e3d6a4a1d9bef50dd5aaece39
Jade Miller <mil19017@byui.edu>
2024-10-07 09:42:51 -0600

    classwork

---
+++ debugging/index.js
@@ -0,0 +1,17 @@
+// const PI = 3.14;
+// let radius = 3;
+// let area = 0;
+// area = radius * radius * PI;
+// console.log(area);
+// radius = 4;
+// area = radius * radius * PI;
+// console.log(area);
+
+const PI = 3.14;
+let area = 0;
+function circleArea(radius) {
+  const area = radius * radius * PI;
+  return area;
+}
+area = circleArea(3);
+console.log(area);
