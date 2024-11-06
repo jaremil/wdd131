8b6d1bea3a43f57ce178b7c04fcee5612eba60fa
Jade Miller <mil19017@byui.edu>
2024-11-06 09:48:16 -0700

    j

---
+++ register/register.js
@@ -0,0 +1,22 @@
+function counting() {
+    let participantsCount = localStorage.getItem('participantsCount');
+
+    if (participantsCount) {
+        participantsCount = parseInt(participantsCount) + 1;
+    } else {
+        participantsCount = 1;
+    }
+    localStorage.setItem('participantsCount', participantsCount);
+    
+    var submissionCount = participantsCount;
+    document.getElementById("submission").innerHTML = `Participants: ${submissionCount}`;
+}
+
+document.getElementById("add").addEventListener("click", counting);
+
+const el = document.querySelector(".participant1");
+let temp = el.outerHTML;
+temp = temp.replace("1", count);
+
+// Code above works but check rubric if it counts
+
