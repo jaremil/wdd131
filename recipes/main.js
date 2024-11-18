24aa54dad52610e3c52f0ebdd316f9ffebe9d392
Jade Miller <mil19017@byui.edu>
2024-11-18 11:27:40 -0700

    k

--- recipes/main.js
+++ recipes/main.js
@@ -1,3 +1,39 @@
+// import recipes from "./recipes.mjs";
+
+// function starRating(recipe) {
+
+//     let counter = 0
+
+//     const rating = document.querySelector(".rating")
+//     let ratingsHTML = '';
+   
+//     for (let i = 0; i < 5; i++) {
+//         if(counter++ < recipe.rating) {
+//             ratingsHTML += '<span aria-hidden="true" class="icon-star">⭐</span>'
+            
+//         } else {
+//             ratingsHTML += '<span aria-hidden="true" class="icon-star-empty">☆</span>'
+//         }
+//     }
+//     return ratingsHTML;
+// }
+
+// function displayRecipe(recipe) {
+//     let recipeHTML = `
+//         <img class="recipeImg" src="${recipe.image}" alt="Recipe Image">
+//         <section class="secondColumn">
+//         <section class="sectionTag"><p class="recipeTag">${recipe.tags}</p></section>
+//         <p class="recipeName">${recipe.name}</p>
+//         <p class="recipeRating">${starRating(recipe)}</p>
+//         <p class="recipeDescription">${recipe.description}</p>
+//         </section>
+//     `;
+//     document.querySelector(".recipeDisplay").innerHTML += recipeHTML;
+// }
+
+// displayRecipe(recipes[0]);
+
+
 import recipes from "./recipes.mjs";
 
 function starRating(recipe) {
@@ -32,9 +68,6 @@ function displayRecipe(recipe) {
 }
 
 displayRecipe(recipes[0]);
-
-
-
 
 
 
