a1ed46cacb65e77b0ec201c7d71c88086557cc24
Jade Miller <mil19017@byui.edu>
2024-12-14 15:08:17 -0700

    mm

---
+++ portfolio/scripts/data.js
@@ -0,0 +1,7 @@
+export const websites = [
+    { name: "Kelli Olsen, Dula", category: "Commerical", technologies: ["HTML", "CSS", "JavaScript"] },
+    { name: "E-commerce Site", category: "Commercial", technologies: ["React", "Node.js"] },
+    { name: "Blog Platform", category: "Content", technologies: ["WordPress", "C#"] },
+    { name: "Weather App", category: "Utility", technologies: ["JavaScript", "API"] }
+  ];
+  
