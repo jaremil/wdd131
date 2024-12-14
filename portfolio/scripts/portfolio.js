33f9ad18477a9682bbad1615827778e10d18ec57
Jade Miller <mil19017@byui.edu>
2024-12-14 14:01:43 -0700

    good

--- portfolio/scripts/portfolio.js
+++ portfolio/scripts/portfolio.js
@@ -1,9 +1,10 @@
 // Each of the following should be used: Objects
-// Each of the following should be used: functions
+// Each of the following should be used: ES Modules
 // Each of the following should be used: Arrays 
 // Each of the following should be used: array method usage
 // Each of the following should be used: forEach
 // Each of the following should be used: map
 // Each of the following should be used: reduce
 // Each of the following should be used: filter
+
 
