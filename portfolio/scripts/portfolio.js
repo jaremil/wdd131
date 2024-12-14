52a742e2fb262936e8098df5166b7a50cbdba335
Jade Miller <mil19017@byui.edu>
2024-12-13 23:36:21 -0700

    m

--- portfolio/scripts/portfolio.js
+++ portfolio/scripts/portfolio.js
@@ -1,30 +1,28 @@
-// At least one image that relates to the content of the page. Working links to
-// your child page(s). Be laid out and styled according to your site plan
-// wireframes. Be valid HTML and CSS Score at least 97 on Lighthouse. The site
-// should have dynamic elements provided by Javascript, your code should show
-// elements of the following: It should show some level of organization: at a
-// minimum it should have more than one function.
-// Each of the following should be used: conditional branching, Objects, ES
-// Modules, Arrays and array method usage (forEach, map, reduce, filter, etc)
+// The site should have dynamic elements provided by Javascript, your code should show elements of the following:
+
+// It should show some level of organization: at a minimum it should have more than one function.
+// It should include DOM interaction: select an element, modify it, and listen for and react to events
+// It should show examples of conditional branching
+// Each of the following should be used: Objects, ES Modules, Arrays and array method usage (forEach, map, reduce, filter, etc)
 
 // DOM interaction: navigation scroll
 
 document.addEventListener('DOMContentLoaded', () => {
-    const links = document.querySelectorAll('a[href^="#"]');
-    
-    links.forEach(link => {
-      link.addEventListener('click', event => {
-        event.preventDefault();
-        
-        const targetId = link.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
-        
-        if (targetElement) {
-          targetElement.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-          });
-        }
-      });
+  const links = document.querySelectorAll('a[href^="#"]'); 
+
+  links.forEach(link => {
+    link.addEventListener('click', event => {
+      event.preventDefault();
+
+      const targetId = link.getAttribute('href').substring(1); 
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        targetElement.scrollIntoView({
+          behavior: 'smooth', 
+          block: 'start', 
+        });
+      }
     });
   });
+});
