98e7d31ecae05479ac54969b061ac9b7873b5dab
Jade Miller <mil19017@byui.edu>
2024-12-14 12:39:41 -0700

    mm

--- portfolio/scripts/portfolio.js
+++ portfolio/scripts/portfolio.js
@@ -1,6 +1,5 @@
 // The site should have dynamic elements provided by Javascript, your code should show elements of the following:
 
-// It should show examples of conditional branching
 // Each of the following should be used: Objects, ES Modules, Arrays and array method usage (forEach, map, reduce, filter, etc)
 
 // DOM interaction: navigation scroll
@@ -24,4 +23,30 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+
+// conditional branching: dark theme
+
+const themeSelector = document.querySelector("#changeToggle");
+
+function changeTheme() {
+
+    var select = document.querySelector("select");
+    var themeValue = select.value; 
+
+if (themeValue == "dark") {
+  document.body.setAttribute('data-theme', 'dark'); 
+
+  document.querySelector('#whiteLogo').setAttribute('src', 'byui-logo_white.png');
+  document.querySelector('#blueLogo').removeAttribute('src');
+  document.querySelector('#blueLogo').removeAttribute('alt');
+  
+  } else {
+    document.body.setAttribute('data-theme', 'light'); 
+    document.querySelector('#blueLogo').setAttribute('src', 'byui-logo_blue.jpg');
+    document.querySelector('#whiteLogo').removeAttribute('src');
+    document.querySelector('#whiteLogo').removeAttribute('alt');
+  }
+}
+
+themeSelector.addEventListener('change', changeTheme);
 
