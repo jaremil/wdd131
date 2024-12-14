184abb7205395e7fa1ed6156a92585296ee3d70e
Jade Miller <mil19017@byui.edu>
2024-12-14 10:03:43 -0700

    mm

--- portfolio/scripts/portfolio.js
+++ portfolio/scripts/portfolio.js
@@ -1,7 +1,5 @@
 // The site should have dynamic elements provided by Javascript, your code should show elements of the following:
 
-// It should show some level of organization: at a minimum it should have more than one function.
-// It should include DOM interaction: select an element, modify it, and listen for and react to events
 // It should show examples of conditional branching
 // Each of the following should be used: Objects, ES Modules, Arrays and array method usage (forEach, map, reduce, filter, etc)
 
@@ -26,3 +24,4 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+
